f447d9806724ad77bfc5f6451bdce30073a2eb81
Jinatey <95699786+Jinatey@users.noreply.github.com>
2022-04-22 14:29:54 +0500

    add todos add line to done todos

--- src/App.jsx
+++ src/App.jsx
@@ -2,38 +2,82 @@ import { useState } from 'react';
 import './App.css';
 
 function App() {
-  const [tasks, setTasks] = useState([
-    { id: 1, taskName: 'jog around,', completed: false },
-    { id: 2, taskName: '10 minutes medication,', completed: false },
-    { id: 3, taskName: 'Read for 1 hour,', completed: false },
-    { id: 4, taskName: 'Pick up groceries,', completed: false },
+  const [text, setText] = useState('');
+
+  const [todos, setTodos] = useState([
+    { id: 1, title: 'jog around,', completed: false },
+    { id: 2, title: '10 minutes medication,', completed: false },
+    { id: 3, title: 'Read for 1 hour,', completed: false },
+    { id: 4, title: 'Pick up groceries,', completed: false },
     {
       id: 5,
-      taskName: 'Complete Todo App on frontend mentor,',
+      title: 'Complete Todo App on frontend mentor,',
       completed: false,
     },
   ]);
+  const completedTodos = todos.filter((todo) => !todo.completed);
+  console.log(completedTodos);
 
   return (
     <>
+    
       <header>
         <div className='hpic'>{/* <img src='/h-back.jpg' alt='' /> */}</div>
         <h1 className='header-text'>T O D O</h1>
       </header>
 
       <div className='container'>
-        {tasks.map((task) => (
-          
-          
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            const updatedT = [
+              ...todos,
+              { title: text, id: Date.now(), completed: false },
+            ];
+            setTodos(updatedT);
+            setText('');
+          }}
+          className='add-form'
+        >
+          <div className=' form-control input'>
+            <input
+              value={text}
+              onChange={(e) => setText(e.target.value)}
+              type='text'
+              placeholder='Create a new task'
+            />
+
+            <button type='submit' className='btn'>
+              ADD
+            </button>
+          </div>
+        </form>
+
+        {todos.map((todo) => (
           <div
-    
-            className='task'
-            key={task.id}
+            onClick={() => {
+              const newList = todos.map((newArrayTodo) =>
+                newArrayTodo.id === todo.id
+                  ? { ...newArrayTodo, completed: !newArrayTodo.completed }
+                  : newArrayTodo
+              );
+              setTodos(newList);
+              // console.log(newList);
+            }}
+            className={`task${todo.completed ? 'container task line' : ''}`}
+            key={todo.id}
           >
-            <input className='checkbox' type='checkbox' />{' '}
-            <span>{task.taskName}</span>
+            <input
+              checked={todo.completed}
+              // id={`todo-${todo.id}`}
+              className='checkbox'
+              type='checkbox'
+            />{' '}
+            <label>{todo.title}</label>
           </div>
         ))}
+
+        <p> Things to do {completedTodos.length}</p>
       </div>
     </>
   );
